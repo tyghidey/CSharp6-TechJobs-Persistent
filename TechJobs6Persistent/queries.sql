adb14dcfbedb6a3d24c577916e38413c34af398f
Courtney Frey <courtneyb.frey@gmail.com>
2023-04-04 10:30:08 -0500

    queries.sql added to project

---
+++ TechJobs6Persistent/queries.sql
@@ -0,0 +1,10 @@
+﻿-- Capture your answer here for each "Test It With SQL" section of this assignment
+    -- write each as comments
+
+
+--Part 1: List the columns and their data types in the Jobs table.
+
+--Part 2: Write a query to list the names of the employers in St. Louis City.
+
+--Part 3: Write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
+    --If a skill does not have a job listed, it should not be included in the results of this query.
